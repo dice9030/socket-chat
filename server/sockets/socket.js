18ec12cb97eb2f334299834134f5be5e9160aa20
dcelis <dcelis@sofdynamic.org>
2019-07-28 19:24:32 -0500

    chat

---
+++ server/sockets/socket.js
@@ -0,0 +1,43 @@
+const { io } = require('../server');
+
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+    client.on('entrarChat', (data, callback) => {
+
+        if (!data.nombre || !data.sala) {
+            return {
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            };
+        }
+        //crear sala
+        client.join(data.sala);
+        usuarios.agregarPersona(client.id, data.nombre,data.sala);
+
+        client.broadcast.to(data.sala).emit('listaPersona',usuarios.getPersonaPorSala(data.sala))
+        callback(usuarios.getPersonaPorSala(data.sala));
+    })
+
+    client.on('crearMensaje', (data) =>{
+        let persona = usuarios.getPersona(client.id)
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje',mensaje)
+    })
+
+    client.on('disconnect',()=>{
+        let personaBorrada= usuarios.borrarPersona(client.id)
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje',crearMensaje('Administrador',`${personaBorrada.nombre} salio de la sala`))
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona',usuarios.getPersonaPorSala(personaBorrada.sala))
+    })
+
+    //Mensaje privado
+    client.on('mensajePrivado',(data) =>{
+            let persana = usuarios.getPersona(client.id);
+            client.broadcast.to(data.para).emit('mensajePrivado',crearMensaje(persana.nombre,data.mensaje));
+    })
+
+
+});
